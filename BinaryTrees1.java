632306763774bf74f560b384979801b33b0a584a
Nitin Pandey <132806870+Nitinxtech@users.noreply.github.com>
2024-06-17 23:20:16 +0530

    Add files via upload

--- BinaryTrees1.java
+++ BinaryTrees1.java
@@ -262,6 +262,137 @@ public class BinaryTrees1 {
             printKthLevel(root.left, level+1, k);
             printKthLevel(root.right, level+1, k);
         }
+
+        //Lowest Common Ancestors -> Approach 1 -> O(n) Tc & O(n) Sc
+        public static boolean getPath(Node root, int n, ArrayList<Node> path) {
+            if (root == null) {
+                return false;
+            }
+
+            path.add(root);
+            if (root.data == n) {
+                return true;
+            }
+
+            boolean foundLeft = getPath(root.left, n, path);
+            boolean foundRight = getPath(root.right, n, path);
+
+            if (foundLeft || foundRight) {
+                return true;
+            }
+            path.remove(path.size()-1);
+            return false;
+        }
+
+        public static Node lca(Node root, int n1, int n2) {
+            ArrayList<Node> path1 = new ArrayList<>();
+            ArrayList<Node> path2 = new ArrayList<>();
+
+            getPath(root, n1, path1);
+            getPath(root, n2, path2);
+
+            //Last Common Ancestor
+            int i=0;
+            for (; i<path1.size() && i<path2.size(); i++) {
+                if (path1.get(i) != path2.get(i)) {
+                    break;
+                }
+            }
+
+            //Last Equal Node -> i-1th
+            Node lca = path1.get(i-1);
+            return lca;
+        }
+
+        //Lowest Common Ancestor -> Approach 2 -> Tc O(n) & Sc O(1)[if we don't consider the recursion]
+        public static Node lca2(Node root, int n1, int n2) {
+            if (root == null || root.data == n1 || root.data == n2) {
+                return root;
+            }
+            Node leftLca = lca2(root.left, n1, n2);
+            Node rightLca = lca2(root.right, n1, n2);
+
+            //leftLca = val & rightLca = null
+            if (rightLca == null) {
+                return leftLca;
+            }
+            //rightLca = val & leftLca = null
+            if (leftLca == null) {
+                return rightLca;
+            }
+            return root;
+        }
+
+        //Minimum Distance Between Nodes -> O(n) Tc
+        public static int lcaDist(Node root, int n) {
+            if (root == null) {
+                return -1;
+            }
+            if (root.data == n) {
+                return 0;
+            }
+
+            int leftDist = lcaDist(root.left, n);
+            int rightDist = lcaDist(root.right, n);
+
+            if (leftDist == -1 && rightDist == -1) {
+                return -1;
+            } else if (leftDist == -1) {
+                return rightDist + 1;
+            } else {
+                return leftDist + 1;
+            }
+        }
+        public static int minDistBwNodes(Node root, int n1, int n2) {
+            Node lca = lca2(root, n1, n2);
+
+            int distance1 = lcaDist(lca, n1);
+            int distance2 = lcaDist(lca, n2);
+
+            return distance1 + distance2;
+        }
+
+        //Kth Ancestor -> O(n) Tc
+        public static int kAncestor(Node root, int n, int k) {
+            if (root == null) {
+                return -1;
+            }
+
+            if (root.data == n) {
+                return 0;
+            }
+
+            int leftDist = kAncestor(root.left, n, k);
+            int rightDist = kAncestor(root.right, n, k);
+
+            if (leftDist == -1 && rightDist == -1) {
+                return -1;
+            }
+
+            int max = Math.max(leftDist, rightDist);
+            if (max+1 == k) {
+                System.out.println(root.data);
+            }
+            return max+1;
+        }
+
+        //Transform To Sum Tree -> O(n)
+        public static int transform(Node root) {
+            if (root == null) {
+                return 0;
+            }
+
+            int leftChild = transform(root.left);
+            int rightChild = transform(root.right);
+
+            int data = root.data;
+
+            int newLeft = root.left == null ? 0 : root.left.data;
+            int newRight = root.right == null ? 0 : root.right.data;
+
+            root.data = newLeft + leftChild + newRight + rightChild;
+            return data;
+        }
     }
     public static void main(String[] args) {
 //        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
@@ -293,6 +424,17 @@ public class BinaryTrees1 {
 //        System.out.println(BinaryTree.isSubtree(root, subRoot));
 //        BinaryTree.topViewOfTree(root);
 
-        BinaryTree.printKthLevel(root, 1, 2);
+//        BinaryTree.printKthLevel(root, 1, 2);
+
+//        int n1 = 4, n2 = 6;
+//        System.out.println(BinaryTree.lca(root, n1, n2).data);
+//        System.out.println(BinaryTree.lca2(root, n1, n2).data);
+//        System.out.println(BinaryTree.minDistBwNodes(root, n1, n2));
+
+//        int n = 5, k = 2;
+//        BinaryTree.kAncestor(root, n, k);
+
+        BinaryTree.transform(root);
+        BinaryTree.preOrder(root);
     }
 }
